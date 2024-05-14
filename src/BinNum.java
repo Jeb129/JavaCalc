ac691dbc2d3b1dcfacee3e419e2bafb982cd6fbf
Ilya Balagurov <elyer129@gmail.com>
2024-05-15 02:57:27 +0300

    Поправил числа с 0

--- src/BinNum.java
+++ src/BinNum.java
@@ -1,8 +1,8 @@
 public class BinNum extends Num {
     public BinNum(String val){
-        super(val, 2, "^[-]?[0-1]+$");
+        super(val, 2, "^[-]?1[0-1]?+$");
     }
     public BinNum(int val){
-        super(val, 2, "^[-]?[0-1]+$");
+        super(val, 2, "^[-]?1[0-1]?+$");
     }
 }
--- src/OctNum.java
+++ src/OctNum.java
@@ -1,8 +1,8 @@
 public class OctNum extends Num {
     public OctNum(String val) {
-        super(val, 8, "^[-]?[0-7]+$");
+        super(val, 8, "^[-]?[1-7][0-7]?+$");
     }
     public OctNum(int val) {
-        super(val, 8, "^[-]?[0-7]+$");
+        super(val, 8, "^[-]?[1-7][0-7]?+$");
     }
 }
